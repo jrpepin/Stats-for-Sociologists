b89fdda4d4d8d1f3bafe6cd11f1a76b0f24078bf
Joanna Pepin <j.pepin@utoronto.ca>
2025-10-09 10:19:52 -0400

    auto exam with versions

--- midterm/midterm-2025F.R
+++ midterm/midterm-2025F.R
@@ -1,8 +1,11 @@
-# MIDTERM ----------------------------------------------------------------------
+# Setup ------------------------------------------------------------------------
 
 ## load packages
 library("here")
 library("exams")
+library("quarto")
+library("qpdf")
+
 
 ## a list of vectors of exam questions in R/Markdown (.Rmd) format
 
@@ -18,20 +21,67 @@ myexam <- list(c(
   here("midterm", "week05", "double-colon.Rmd")
 ))
 
-## Create form codes -----------------------------------------------------------
-form_codes <- list(
-  list(FormCode = "A"),
-  list(FormCode = "B")
-)
+# Create exam versions ---------------------------------------------------------
 
-## exams2pdf -------------------------------------------------------------------
-
-midterm <- exams2pdf(
+exams2pdf(
   myexam, 
   n = 2, # number of exam versions
   nsamp = 9, # number of exam questions
-  name = "midterm_2025F", 
-  dir = here("midterm"),
+  name = "midterm_", 
+  dir = here("docs", "midterm"),
   template="myexam2",
-  control = form_codes
+  title = NULL # merge with own later
   )
+
+# Create title pages -----------------------------------------------------------
+
+quarto_render(
+  input = here("midterm", "title-page.qmd"),
+  output_file = "title_1.pdf",
+  execute_params = list(form_code = "A", captions = c("Intro caption"))
+)
+
+quarto_render(
+  input = here("midterm", "title-page.qmd"),
+  output_file = "title_2.pdf",
+  execute_params = list(form_code = "B", captions = c("Instructions caption"))
+)
+
+# Combine documents ------------------------------------------------------------
+
+## Form A
+pdf_combine(
+  input = c(
+    here("docs", "midterm", "title_1.pdf"),
+    here("docs", "midterm", "midterm_1.pdf")
+  ),
+  output = here("docs", "midterm", "midterm_A_AK.pdf")
+)
+
+## Form B
+pdf_combine(
+  input = c(
+    here("docs", "midterm", "title_2.pdf"),
+    here("docs", "midterm", "midterm_2.pdf")
+  ),
+  output = here("docs", "midterm", "midterm_B_AK.pdf")
+)
+
+# Exams without the answer key -------------------------------------------------
+
+# Get total number of pages
+n_pages_A <- pdf_length(here("docs", "midterm", "midterm_A_AK.pdf"))
+n_pages_B <- pdf_length(here("docs", "midterm", "midterm_B_AK.pdf"))
+
+# Keep all pages except the last
+pdf_subset(
+  input = here("docs", "midterm", "midterm_A_AK.pdf"),
+  pages = 1:(n_pages_A - 1),
+  output = here("docs", "midterm", "midterm_A.pdf")
+)
+
+pdf_subset(
+  input = here("docs", "midterm", "midterm_B_AK.pdf"),
+  pages = 1:(n_pages_B - 1),
+  output = here("docs", "midterm", "midterm_B.pdf")
+)
